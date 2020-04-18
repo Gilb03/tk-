e4aeeccf6be43519c73d32b9058b33fe0363fe3f
Gilbert King <gilbertking91@gmail.com>
2020-04-18 15:38:53 -0500

    first commit

---
+++ button.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+
+
+button = tk.Button(frame, 
+                   text="CLICK ME", 
+                   fg="red",
+                   command=lambda: print("Hello"))
+
+
+
+
+button.pack(side=tk.LEFT) 
+root.mainloop() 
